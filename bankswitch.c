9e29e0117ab91b9b7eb3f25c877d14fc0ab9e48d
Guto Maia <guto@guto.net>
2021-05-02 12:07:56 -0300

    feat: using chr-ram

--- bankswitch.c
+++ bankswitch.c
@@ -11,14 +11,12 @@ PRG ROM segments (CODE0-CODE6, CODE).
 // bank-switching configuration
 #define NES_MAPPER 4        // Mapper 4 (MMC3)
 #define NES_PRG_BANKS 4     // # of 16KB PRG banks
-#define NES_CHR_BANKS 8     // # of 8KB CHR banks
+#define NES_CHR_BANKS 0     // # of 8KB CHR banks
 
 #include <peekpoke.h>
 #include <string.h>
 #include "neslib.h"
 
-// link the pattern table into CHR ROM
-//#link "chr_generic.s"
 
 #define MMC_MODE 0x00
 
@@ -26,8 +24,6 @@ PRG ROM segments (CODE0-CODE6, CODE).
     POKE(0x8000, MMC_MODE|(r));\
     POKE(0x8001, (n));
 
-#define MMC3_CHR_0000(n) MMC3_SET_REG(0,n)
-#define MMC3_CHR_0800(n) MMC3_SET_REG(1,n)
 #define MMC3_CHR_1000(n) MMC3_SET_REG(2,n)
 #define MMC3_CHR_1400(n) MMC3_SET_REG(3,n)
 #define MMC3_CHR_1800(n) MMC3_SET_REG(4,n)
@@ -55,17 +51,40 @@ void draw_text(word addr, const char* text) {
 }
 
 // back to main code segment
+#pragma rodata-name("CODE")
 #pragma code-name("CODE")
+
+// link the pattern table into CHR ROM
+//#link "chr_generic.s"
+extern byte * chars[];
+
+
+void load_chars(){
+  byte *tile = ((byte*) chars);
+  int i=0;
+  int j=0;
+
+  ppu_off();
+  vram_adr(0);
+  for (i=0; i < 128; i++) {
+    for (j=0; j< 64; j++) {
+      vram_put(*tile);
+      tile++;
+    }
+  }
+  ppu_on_all();
+}
 
 void main(void)
 {
   // set palette colors
+  load_chars();
   pal_col(1,0x04);
   pal_col(2,0x20);
   pal_col(3,0x30);
   // setup CHR bank switching for background to first 2 2K banks
-  MMC3_CHR_0000(0);
-  MMC3_CHR_0800(2);
+  //MMC3_CHR_0000(0);
+  //MMC3_CHR_0800(2);
   // select bank 0 in $8000-$9fff
   MMC3_PRG_8000(0);
   vram_adr(NTADR_A(2,2));
